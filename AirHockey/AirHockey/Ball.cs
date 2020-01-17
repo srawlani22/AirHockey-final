852b1f84e0d213896d0b043259429c46df3f9884
Sparsh Rawlani <srawlani22@my.whitworth.edu>
2020-01-17 13:42:56 -0800

    added the canvas and the ball

---
+++ AirHockey/AirHockey/Ball.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Converters;
+using System.Windows.Shapes;
+
+namespace AirHockey
+{
+    class Ball
+    {
+        Ellipse ellip = new Ellipse();
+        
+        public int width { get; set; }
+        public int height { get; set; }
+
+        public int size { get; set; }
+
+        int mov = 5;
+
+        int xaxis, yaxis;
+
+        public Ball(Canvas c)
+        {
+            SolidColorBrush BallColor = new SolidColorBrush();
+            width = 80;
+            height = 80;
+            size = 25;
+            xaxis = mov;
+            yaxis = mov;
+            BallColor.Color = Color.FromRgb(255, 0, 0);
+
+            ellip.Fill = BallColor;
+            ellip.Width = size;
+            ellip.Height = size;
+            Canvas.SetTop(ellip, height);
+            Canvas.SetLeft(ellip,width);
+            c.Children.Add(ellip);
+        }
+
+        public void move()
+        {
+            width += xaxis;
+            height += yaxis;
+            Canvas.SetTop(ellip,height);
+            Canvas.SetLeft(ellip,width);
+        }
+    }
+}
---
+++ AirHockey/AirHockey/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+
+namespace AirHockey
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Ball playBall;
+
+        DispatcherTimer dt;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            dt = new DispatcherTimer();
+            dt.Interval = new TimeSpan(0, 0, 0, 1);
+            dt.Tick += dt_tick;
+            playBall = new Ball(canvas);
+            
+        }
+
+        public void dt_tick(object obj, EventArgs e)
+        {
+            
+            playBall.move();
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (dt.IsEnabled)
+            {
+                dt.IsEnabled = true;
+                dt.Start();
+            }
+            else
+            {
+                dt.IsEnabled = false;
+                dt.Stop();
+            }
+        }
+    }
+}
